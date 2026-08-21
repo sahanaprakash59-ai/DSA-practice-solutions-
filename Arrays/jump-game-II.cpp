90eb0a9cd97bdb10d6cb18868f99ba3e4ce708c1
sahanaprakash50-ai <your-github-sahanaprakash59@gmail.com>
2026-08-21 09:09:18 +0530

    LeetCode 45. Jump Game II

---
+++ Arrays/jump-game-II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int farthest=0, currentEnd=0, jumps=0;
+        for(int i=0; i<nums.size()-1; i++){
+            farthest=max(farthest,i+nums[i]);
+            if(i==currentEnd){
+                jumps++;
+                currentEnd=farthest;
+            } 
+        }
+        return jumps;    
+    }
